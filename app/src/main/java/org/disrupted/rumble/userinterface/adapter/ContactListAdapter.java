04d4e1d9724211f81249d99a529a07f20780ec21
Marlinski <marlinski@disruptedsystems.org>
2015-09-25 14:31:51 +0200

    * added contact list

---
+++ app/src/main/java/org/disrupted/rumble/userinterface/adapter/ContactListAdapter.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2014 Disrupted Systems
+ * This file is part of Rumble.
+ * Rumble is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rumble is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Rumble.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.disrupted.rumble.userinterface.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
+import com.squareup.picasso.Picasso;
+
+import org.disrupted.rumble.R;
+import org.disrupted.rumble.database.objects.Contact;
+import org.disrupted.rumble.userinterface.events.UserDeleteContact;
+
+import java.util.ArrayList;
+
+import de.greenrobot.event.EventBus;
+
+/**
+ * @author Marlinski
+ */
+public class ContactListAdapter extends BaseAdapter {
+
+    public static final String TAG = "ContactListAdapter";
+
+    private Activity activity;
+    private LayoutInflater inflater;
+    private ArrayList<Contact> contactList;
+    private static final TextDrawable.IBuilder builder = TextDrawable.builder().rect();
+
+
+    public ContactListAdapter(Activity activity) {
+        this.activity = activity;
+        this.inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.contactList = null;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.contact_list_item, null, true);
+
+        ImageView contact_avatar = (ImageView)    layout.findViewById(R.id.contact_avatar);
+        TextView  contact_name   = (TextView)     layout.findViewById(R.id.contact_name);
+        TextView  contact_uid    = (TextView)     layout.findViewById(R.id.contact_uid);
+        ImageView contact_delete = (ImageView)    layout.findViewById(R.id.contact_delete);
+
+        ColorGenerator generator = ColorGenerator.DEFAULT;
+        contact_avatar.setImageDrawable(
+                builder.build(contactList.get(i).getName().substring(0, 1),
+                        generator.getColor(contactList.get(i).getUid())));
+        contact_name.setText(contactList.get(i).getName());
+        contact_uid.setText("Contact ID: "+contactList.get(i).getUid());
+
+        /*
+         * Manage click events
+         */
+        final String    uid      = contactList.get(i).getUid();
+        contact_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventBus.getDefault().post(new UserDeleteContact(uid));
+            }
+        });
+
+        return layout;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        if(contactList == null)
+            return 0;
+        if(contactList.isEmpty())
+            return 0;
+        return i;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return contactList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        if(contactList == null)
+            return 0;
+        else
+            return contactList.size();
+    }
+
+    public void swap(ArrayList<Contact> contactList) {
+        if(this.contactList != null)
+            this.contactList.clear();
+        this.contactList = contactList;
+        notifyDataSetChanged();
+    }
+
+}
--- app/src/main/java/org/disrupted/rumble/userinterface/fragments/FragmentContactList.java
+++ app/src/main/java/org/disrupted/rumble/userinterface/fragments/FragmentContactList.java
@@ -17,7 +17,28 @@
 
 package org.disrupted.rumble.userinterface.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.disrupted.rumble.R;
+import org.disrupted.rumble.database.DatabaseExecutor;
+import org.disrupted.rumble.database.DatabaseFactory;
+import org.disrupted.rumble.database.events.ContactDeletedEvent;
+import org.disrupted.rumble.database.events.ContactInsertedEvent;
+import org.disrupted.rumble.database.objects.Contact;
+import org.disrupted.rumble.userinterface.adapter.ContactListAdapter;
+
+import java.util.ArrayList;
+
+import de.greenrobot.event.EventBus;
 
 /**
  * @author Marlinski
@@ -25,5 +46,73 @@ import android.support.v4.app.Fragment;
 public class FragmentContactList  extends Fragment {
 
     public static final String TAG = "FragmentContactList";
+
+
+    private View mView;
+    private ListView contactList;
+    private ContactListAdapter contactListAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_group_list, container, false);
+        contactList = (ListView) mView.findViewById(R.id.group_list);
+        contactListAdapter = new ContactListAdapter(getActivity());
+        contactList.setAdapter(contactListAdapter);
+        EventBus.getDefault().register(this);
+        getContactList();
+        return mView;
+    }
+
+    @Override
+    public void onDestroy() {
+        if(EventBus.getDefault().isRegistered(this))
+            EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.group_list_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getContactList() {
+        DatabaseFactory.getContactDatabase(getActivity()).getContacts(onContactsLoaded);
+    }
+    private DatabaseExecutor.ReadableQueryCallback onContactsLoaded = new DatabaseExecutor.ReadableQueryCallback() {
+        @Override
+        public void onReadableQueryFinished(final Object result) {
+            if(getActivity() == null)
+                return;
+            getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ArrayList<Contact> answer = (ArrayList<Contact>)(result);
+                        contactListAdapter.swap(answer);
+                    }
+                }
+            );
+        }
+    };
+
+    public void onEvent(ContactInsertedEvent event) {
+        getContactList();
+    }
+    public void onEvent(ContactDeletedEvent event) {
+        getContactList();
+    }
 
 }
